8405d11531c21f15365737296223a3f6fa365c94
naon708 <naoya_nis@icloud.com>
2023-04-01 16:31:46 +0900

    ImageMagick以外のライブラリを使う際の記述

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module PortraitsUploader
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # libvipsなど他のライブラリを使用する場合は以下のオプションを追加する
+    # config.active_storage.variant_processor = :vips
   end
 end
